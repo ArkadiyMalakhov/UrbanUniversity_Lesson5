737362e65ebe217be313d01412d6e067de46dcee
AMalakhov <spamarkady@yandex.ru>
2024-05-23 21:59:00 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,10 @@
+immutable_var = 1, 2, 3, "Four", "Five", True
+print(immutable_var)
+# immutable_var[3] = 4
+# print(immutable_var)
+# Tип данных tuple не поддерживает изменение объектов содержащихся в нем,
+# если объекты относятся к неизменяемому типу данных
+mutable_var = [1, 2, 3, "Four", "Five", True]
+print(mutable_var)
+mutable_var[3] = 4
+print(mutable_var)
